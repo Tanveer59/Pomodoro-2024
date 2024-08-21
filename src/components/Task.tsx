686973da36d25e9f74a2123ba58a47b3f0efa1fd
Tanveer59 <tanveer1234awan@gmail.com>
2024-08-21 18:05:30 +0500

    sd

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,4 @@
-import Button from "./Button";
-export default function  (){
+export default function Task(){
     return (
         <div className="relative flex cls gap-2">
             <input type="text" placeholder="Add Task" className="p-2 rounded-lg text-black cls " />  
